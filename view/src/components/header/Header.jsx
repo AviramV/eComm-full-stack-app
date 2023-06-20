afc9f2d5716c5d8c59c5c29ac875cb811bd1b343
AviramV <aviramv2@gmail.com>
2023-06-20 09:05:27 +0300

    add required components

---
+++ view/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <>
+      <header>
+        <div className="logo">LOGO</div>
+        <div className="user">User</div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
