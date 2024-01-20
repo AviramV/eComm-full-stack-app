e847b797e3462487b6b015c356c1be6f82c4c99a
AviramV <aviramv2@gmail.com>
2024-01-20 23:12:03 +0200

    Add log out button

--- view/src/components/header/Header.jsx
+++ view/src/components/header/Header.jsx
@@ -4,27 +4,46 @@ import { useContext } from "react";
 import { CurrentUserContext } from "../../Contexts";
 
 function Header() {
-  const { currentUser } = useContext(CurrentUserContext);
+  const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
+
+  const logOut = async () => {
+    const serverURL = import.meta.env.VITE_SERVER_BASE_URL;
+    const response = await fetch(`${serverURL}/logout`, {
+      method: "POST",
+      credentials: "include",
+    });
+    setCurrentUser({});
+    console.log("from header: ", await response.text());
+  };
+
   return (
     <header>
       <nav>
         <NavLink className="logo" to="/">
           LOGO
         </NavLink>
-        {currentUser ? (
-          <NavLink className="user" to="/user">
-            {currentUser.username}
-          </NavLink>
-        ) : (
-          <div style={{ display: "flex", gap: "1em" }}>
-            <NavLink className="login" to="/login">
-              Login
-            </NavLink>
-            <NavLink className="register" to="/register">
-              Register
-            </NavLink>
-          </div>
-        )}
+
+        <div style={{ display: "flex", gap: "1em", alignItems: "center" }}>
+          {currentUser.username ? (
+            <>
+              <NavLink className="user" to="/user">
+                {currentUser.username}
+              </NavLink>
+              <NavLink to="/">
+                <button onClick={logOut}>Log Out</button>
+              </NavLink>
+            </>
+          ) : (
+            <>
+              <NavLink className="login" to="/login">
+                <button>Login</button>
+              </NavLink>
+              <NavLink className="register" to="/register">
+                Register
+              </NavLink>
+            </>
+          )}
+        </div>
       </nav>
     </header>
   );
